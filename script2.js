3fc2ffd5271b7bd20b40b22a98ea6e374c2915a2
John Daniel Bossér <daniel@bosser.com>
2019-07-05 16:07:54 +0200

    Bunch of comments

--- script2.js
+++ script2.js
@@ -9,6 +9,7 @@ const getAspectRatio = (canvas) => canvas.clientWidth / canvas.clientHeight
 // Helper function to get the camera. 
 // Can redifine some of the camera properies here.
 const getPerspectiveCamera = canvas => {
+
     const fov = 75;
     const aspect = getAspectRatio(canvas);
     const near = 0.1;
@@ -17,9 +18,12 @@ const getPerspectiveCamera = canvas => {
     camera.position.y = 50;
     camera.up.set(0,0,1);
     return camera;
-}
 
+};
+
+// Helper function to get a Orthographic camera. 
 const getOrthographicCamera = canvas => {
+
     const width = canvas.clientWidth/10; 
     const height = canvas.clientHeight/10;
     const near = 0.01;
@@ -28,34 +32,54 @@ const getOrthographicCamera = canvas => {
     const camera = new THREE.OrthographicCamera(
         width / - 2, width / 2, 
         height / 2, height / - 2);
-
      
     camera.position.y = 50;
     camera.up.set(0,0,1);
     return camera;
+
 };
 
-const getCamera = canvas => {
-    return getOrthographicCamera(canvas);  
-};
+const getCamera = canvas => getOrthographicCamera(canvas);  
 
+
+// This function makes sure that the renderer have the same size as the camera. 
+// The "false" here makes sure that three.js does not override the size of the canvas itself. 
+// if the boolean is set to true, it will override the css-style for the canvas. 
+// This function is also used when we rescale our browser window, as will be seen furter down in the code.
 const setRendererSize = (renderer, canvas) => renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
 
 // Set up all the master elements, such as the scene, renderer and more
+
+// This is the html element in which we draw in. This element can be 
+// resized and shift aspect ratio. 
 const canvas = document.getElementById("c");
+
+// The scene is where we have all of our object and lights. 
+// Since we are working with our own shaders in this demo, no lights will be added
+// since the "lights" (the audio source) is defined inside of the shaders. 
 const scene  = new THREE.Scene();
-//scene.background = new THREE.Color(0x000000);
-scene.background = new THREE.Color();
+scene.background = new THREE.Color(0x000000); // Set the shader background to black.
+
+// The renderer is the element that draws our 3D objects to our canvas. 
 const renderer = new THREE.WebGLRenderer({canvas});
-setRendererSize(renderer, canvas);
+
+setRendererSize(renderer, canvas); // We want the renderer to have the same size as our canvas
+                                   // to get maximal resolution.  
+
+// Get the camera.
 const camera = getCamera(canvas);
-const light = new THREE.DirectionalLight(0xFFFFFF,1); // Color white, and intensity 1 are the defaults. 
-light.position.y = 300;
-scene.add(light);
+
+// Draw in the canvas what the camera can see. Should be nothing right now, since
+// we have no elements in the scene. 
 renderer.render(scene, camera);
 
-// Bind some functions to resize event of browser, to rescale the view
+// These couple of lines defines a function to execute when the browser
+// window is resized.  
+// If the browser window is resized, our canvas will have a height and width in
+// px where we are able to draw. This means that both resolution and aspect
+// ratio can change. 
 window.addEventListener('resize', () => {
+
     // Update camera aspect ratio
     camera.aspect = getAspectRatio(canvas);
     camera.updateProjectionMatrix();
@@ -65,15 +89,24 @@ window.addEventListener('resize', () => {
 
     // Repaint
     renderer.render(scene, camera);
+
 });
 
-
+// If a model changes material we need to repaint the scene.  
 const replaceMaterial = (model, material) => {
+
     model.material = material;
     renderer.render(scene, camera);    
+
 };
 
+// Function to give the model a phase material. This material is 
+// "sensitive" to changes in the aucustic phase. 
+// NOTE: this function is not done yet. 
+// TODO: incorporate the wavelength and phaseshift (the phase) 
+// calculating the phase/angle. 
 const setPhaseMaterial = (model, lambda, phase = 0) => {
+
     // Playground for the different shaders
     // Mostly taken from 
     // https://dev.to/maniflames/creating-a-custom-shader-in-threejs-3bhi
@@ -137,23 +170,28 @@ const setPhaseMaterial = (model, lambda, phase = 0) => {
     let inverseRotationMatrix = new THREE.Matrix4();
     inverseRotationMatrix.makeRotationFromEuler(model.rotation);
     const customMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-        colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
-        colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)},
-        inverseRotationMatrix: {type: 'mat4', value: inverseRotationMatrix}
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader  
+
+        uniforms: {
+
+            colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+            colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)},
+            inverseRotationMatrix: {type: 'mat4', value: inverseRotationMatrix}
+
+        },
+        fragmentShader: fragmentShader,
+        vertexShader: vertexShader  
+
     });
 
-    
     replaceMaterial(model, customMaterial);
+
 };
 
 let model;
 const loader = new STLLoader();
 // Load the russian submarine
 loader.load('./ShaderFood/P677_shell(fine).stl', geometry => {
+
     geometry.computeFaceNormals();
     geometry.computeVertexNormals();
     //geometry.rotateX(-Math.PI/2); 
@@ -166,8 +204,8 @@ loader.load('./ShaderFood/P677_shell(fine).stl', geometry => {
     renderer.render(scene, camera);
     model = submarine;
     setPhaseMaterial(model, 1, 1); 
-});
 
+});
 
 // Add orbit controls
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -180,66 +218,97 @@ controls.addEventListener('change', () => {});
 
 // Animation loop for the orbit controls, and pretty much everything
 const animate = () => {
+
     renderer.render(scene, camera);
     controls.update();
     requestAnimationFrame(animate);    
-}
+};
 animate();
 
+// The following functions are exposing different 
+// properties to the ui. 
 
-// Exporting functions to the ui
+// Make it possible for the ui to respond to event when someone 
+// moves the camera. This is useful to update the coordinates for 
+// a camera controller when someone changes the camera position with mouse. 
 const setCameraChangeListener = handler => {
+
     controls.addEventListener('change', handler);    
+
 }
 
+// Start (or stop) OrbitControls auto rotation around the
+// target.
 const setAutoRotation = bool => controls.autoRotate = bool;
 
+// Move camera in space. It will still though focus on the same target after
+// the move.
 const setCameraPosition = (x,y,z) => {
+
     camera.position.set(x,y,z);    
     controls.update();
+
 }
 
+// Change camera focus and what to orbit around when autorotate. 
 const setCameraLookAt = (x,y,z) => {
+
     controls.target = new THREE.Vector3(x,y,z);
     controls.update();
-}
 
+};
+
+// Move the model in the world.
 const setModelPosition = (x,y,z) => {
-    model.position.set(x,y,z);
-} 
 
+    model.position.set(x,y,z);
+}; 
+
+// Rotate the model in the world.
 const setModelRotation = (x,y,z) => {
+
     model.rotation.set(x,y,z);
+    
     // Rotate inside shader
+    // this is needed, since the incoming aucustic wave 
+    // have the model coordinates. We need to "rotate back"
+    // the incoming wave to make sure it stays in place in the world.
+
+    // we do a clone, since object oriented programming is actually quite scary 
+    // sometimes. 
     let rotation = model.rotation.clone();
     rotation.x = -rotation.x;
     rotation.y = -rotation.y;
     rotation.z = -rotation.z;
     model.material.uniforms.inverseRotationMatrix.value.makeRotationFromEuler(rotation);
+    // Lots of dots ... ^ 
 
-    console.log(model);
-}
+};
 
 const replaceModel = imported_model => {
-        //geometry.computeFaceNormals();
-        //geometry.computeVertexNormals();
         
-        //const material = new THREE.MeshLambertMaterial({color: 0xff5533});
-        //const imported_model = new THREE.Mesh(geometry, material);
         scene.remove( model );
         scene.add(imported_model);         
 
         // Repaint
         renderer.render(scene, camera);
+
+        // Make sure that the model points to our new model.
         model = imported_model;
+
 };
 
+// Upload obj.
 const replaceModelOBJ = (uri) => {
+
     const loader = new OBJLoader2();
     loader.load(uri, replaceModel);
+
 };
 
+// Upload STL.
 const replaceModelSTL = (uri) => {
+
     const loader = new STLLoader();
     loader.load(uri, geometry => {
         geometry.computeFaceNormals();
@@ -249,5 +318,8 @@ const replaceModelSTL = (uri) => {
         const imported_model = new THREE.Mesh(geometry, material);
         replaceModel(imported_model);
     });
+
 };
+
+// Export all the setters, getters and setListneres to the ui controller.
 export {setCameraChangeListener, setAutoRotation, setCameraPosition, setCameraLookAt, setModelPosition, setModelRotation, replaceModelSTL, replaceModelOBJ};
