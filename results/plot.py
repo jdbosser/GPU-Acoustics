fc18d9dff65e7656f5aad3d999f4a1e94809cb92
John Daniel Bossér <daniel@bosser.com>
2019-07-16 13:56:51 +0200

    Added extra results for on the 2m radius spehere tests for different buffer resolutions

--- results/plot.py
+++ results/plot.py
@@ -17,3 +17,22 @@ with open(filename, 'r') as f:
     logplot(json_data['x'], json_data['y'])
     plt.savefig("pngs/2msphereDiffFreqTS.png", format="png") 
     plt.savefig("svgs/2msphereDiffFreqTS.png", format="svg") 
+
+filename = "2msphereDiffFreqsTS200x200.json"
+with open(filename, 'r') as f:
+    read_data = f.read()
+    json_data = json.loads(read_data) 
+    plt.clf()
+    logplot(json_data['x'], json_data['y'])
+    plt.savefig("pngs/2msphereDiffFreqTS200x200.png", format="png") 
+    plt.savefig("svgs/2msphereDiffFreqTS200x200.png", format="svg") 
+
+filename = "2msphereDiffFreqsTS2000x2000.json"
+with open(filename, 'r') as f:
+    read_data = f.read()
+    json_data = json.loads(read_data) 
+    plt.clf()
+    logplot(json_data['x'], json_data['y'])
+    plt.savefig("pngs/2msphereDiffFreqTS2000x2000.png", format="png") 
+    plt.savefig("svgs/2msphereDiffFreqTS2000x2000.png", format="svg") 
+
